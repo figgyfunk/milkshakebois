bea4ccec34d98f29136fadc77088aacbaf2d1616
figgyfunk <figa@rpi.edu>
2018-09-06 14:48:36 -0400

    made text scrolling

--- Milkshakebois/Assets/interact.cs
+++ Milkshakebois/Assets/interact.cs
@@ -7,23 +7,19 @@ public class interact : MonoBehaviour {
     public bool touching = false;
     public string text = "";
     public bool active = false;
-    public float heightFromPlayer = 2f;
-    public float scale = 1f;
-    public float textOffset = 0f;
-    public Transform textBox;
-    private Transform box;
-    private Transform player;
+    public GameObject box;
+    public float x;
+    public float y;
+    public float letterPause = .2f;
+    private string printText = "";
     // Use this for initialization
     public void Start()
     {
-        box = Instantiate(textBox, new Vector3(0f, 0f), Quaternion.identity);
-        box.localScale = box.localScale * scale;
         box.GetComponent<MeshRenderer>().enabled = false;
     }
     private void OnTriggerEnter(Collider other)
     {
         touching = true;
-        player = other.gameObject.transform;
     }
     private void OnTriggerExit(Collider other)
     {
@@ -37,10 +33,7 @@ public class interact : MonoBehaviour {
         if(active)
         {
             GUI.enabled = true;
-            Vector3 screenPos = Camera.main.WorldToScreenPoint(new Vector3(box.position.x + (box.localScale.x)- textOffset , box.position.y +(box.localScale.y/2f)));
-            GUIStyle style = GUI.skin.GetStyle("Label");
-            style.alignment = TextAnchor.UpperLeft;
-            GUI.Label(new Rect(Screen.width - screenPos.x , Screen.height - screenPos.y, 130 * scale, 70 * scale), text, style);
+            GUI.Label(new Rect(160f, 2f, x, y), printText);
          
         }
         
@@ -50,11 +43,20 @@ public class interact : MonoBehaviour {
     {
 		if(touching && Input.GetKeyDown(KeyCode.E))
         {
-            box.transform.position = new Vector3(player.position.x, player.position.y + heightFromPlayer);
             box.GetComponent<MeshRenderer>().enabled = true;
             active = true;
-            //Debug.Log(box.transform.position);
+            StartCoroutine(revealText());
         }
 
 	}
+
+    IEnumerator revealText()
+    {
+        foreach(char letter in text.ToCharArray())
+        {
+            printText += letter;
+            yield return new WaitForSeconds(letterPause);
+        }
+        
+    }
 }
