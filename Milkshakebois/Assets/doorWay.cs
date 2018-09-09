0b9dc5da6441f11f3b73f71607168d15b6973b75
figgyfunk <figa@rpi.edu>
2018-09-09 13:19:43 -0400

    transporting/looping with doorways, with a fade in and fadeout implemented as well.

---
+++ Milkshakebois/Assets/doorWay.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorWay : MonoBehaviour {
+
+    public GameObject otherDoorWay;
+    public bool active = false;
+    public GameObject fade;
+    private bool fadeIn = false;
+    private bool fadeOut = false;
+    private GameObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        active = true;
+        player = other.gameObject;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        active = false;
+        player = null;
+    }
+
+    public void Start()
+    {
+        fade.GetComponent<MeshRenderer>().material.color = new Color(0f, 0f, 0f, 0f);
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && active)
+        {
+            
+            
+            active = false;
+            fadeIn = true;
+
+        }
+
+        if (fadeIn)
+        {
+            if(fade.GetComponent<MeshRenderer>().material.color.a < 1f)
+            {
+                float currentAlpha = fade.GetComponent<MeshRenderer>().material.color.a;
+                currentAlpha += .0001f / Time.deltaTime;
+                fade.GetComponent<MeshRenderer>().material.color = new Color(0f, 0f, 0f, currentAlpha);
+            }
+
+            else
+            {
+                player.transform.position = otherDoorWay.transform.position;
+                fadeIn = false;
+                fadeOut = true;
+            }
+        }
+
+        if (fadeOut)
+        {
+            if(fade.GetComponent<MeshRenderer>().material.color.a > 0f)
+            {
+                float currentAlpha = fade.GetComponent<MeshRenderer>().material.color.a;
+                currentAlpha -= .0001f / Time.deltaTime;
+                fade.GetComponent<MeshRenderer>().material.color = new Color(0f, 0f, 0f, currentAlpha);
+            }
+            else
+            {
+                fadeOut = false;
+            }
+        }
+        
+    }
+
+
+}
--- Milkshakebois/Assets/interact.cs
+++ Milkshakebois/Assets/interact.cs
@@ -41,11 +41,15 @@ public class interact : MonoBehaviour {
     // Update is called once per frame
     void Update ()
     {
-		if(touching && Input.GetKeyDown(KeyCode.E))
+		if(touching)
         {
-            box.GetComponent<MeshRenderer>().enabled = true;
-            active = true;
-            StartCoroutine(revealText());
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                box.GetComponent<MeshRenderer>().enabled = true;
+                active = true;
+                StartCoroutine(revealText());
+            }
+            
         }
 
 	}
