c43a2a40774dbe0d1657cce7785ce0a4a4e93293
Pheonix <colecarpenter@gmail.com>
2018-09-11 19:57:47 -0400

    added char work

---
+++ Assets/Scripts/player_controller.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour {
+
+    public float speed = 1f;
+    public float jumpheight = 10f;
+    public Sprite main_cube;
+    public Sprite outline;
+    public GameObject spriteObject;
+    private Rigidbody2D m_Rigidbody;
+    private SpriteRenderer currImage;
+
+	// Use this for initialization
+	void Start () {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+        currImage = spriteObject.GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey("left")) {
+            m_Rigidbody.velocity = -transform.right * speed + new Vector3(0,m_Rigidbody.velocity.y,0);
+        }
+        if(Input.GetKey("right")) {
+            m_Rigidbody.velocity = transform.right * speed + new Vector3(0, m_Rigidbody.velocity.y, 0);
+        }
+        if(Input.GetKeyDown("up")){
+            m_Rigidbody.velocity = transform.up * jumpheight;
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            currImage.sprite = outline;
+        }
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            currImage.sprite = main_cube;
+        }
+    }
+
+}
