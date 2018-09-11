7d9d52dafb8f39312068a17b95c85322331b1bd2
figgyfunk <figa@rpi.edu>
2018-09-10 23:05:17 -0400

    win text will now pop up after touching trigger.

---
+++ Milkshakebois/Assets/youWin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class youWin : MonoBehaviour {
+
+    public bool active;
+    private GUIStyle style;
+    public int fontSize = 100;
+	// Use this for initialization
+	void Start () {
+        active = false;
+        style = new GUIStyle();
+        style.fontSize = fontSize;
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        active = true;
+    }
+
+    private void OnGUI()
+    {
+        if (active)
+        {
+            GUI.Label(new Rect(Screen.width/2f - 50f, 150f, 100f, 100f), "You Win!", style);
+        }
+    }
+}
