aad36fc4b34174ce503e4ae519a89d353dd6cad6
figgyfunk <figa@rpi.edu>
2018-09-19 14:34:36 -0400

    menu scene.

---
+++ Milkshakebois/Assets/sceneSwitcher.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneSwitcher : MonoBehaviour
+{
+    public string nextScene;
+    public void startGame()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+	
+}
